d7affd0b359beb8c09f368c20988a3c898ad96b1
6lnyx <senol-enes@protonmail.com>
2025-10-05 22:49:27 +0300

    fix dark theme icon

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (localStorage.getItem("dark-theme") === "true") {
         document.body.classList.add("dark-theme");
-        icon.src = "images/sun.png"; 
+        icon.src = "/images/sun.png"; 
     } else {
-        icon.src = "images/moon.png"; 
+        icon.src = "/images/moon.png"; 
     }
 
     icon.onclick = function() {
         document.body.classList.toggle("dark-theme");
 
         if (document.body.classList.contains("dark-theme")) {
-            icon.src = "images/sun.png"; 
+            icon.src = "/images/sun.png"; 
             localStorage.setItem("dark-theme", "true"); 
         } else {
-            icon.src = "images/moon.png"; 
+            icon.src = "/images/moon.png"; 
             localStorage.setItem("dark-theme", "false");
         }
     };
